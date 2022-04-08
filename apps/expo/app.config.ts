99d6bc446780cc4aebfadc3003a9f7122f900feb
Fernando Rojo <frojo@sas.upenn.edu>
2022-04-08 17:44:13 -0400

    oops

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -1,18 +1,16 @@
-{
-  "expo": {
-    "name": "solito-eas",
-    "slug": "solito-eas",
-    "version": "1.0.0",
-    "sdkVersion": "44.0.0",
-    "scheme": "solito-eas",
-    "platforms": ["ios"],
-    "runtimeVersion": "1",
-    "updates": {
-      "url": "https://u.expo.dev/7e5b02b4-05a1-4c41-b65f-c9e09f685206"
-    },
-    "ios": {
-      "bundleIdentifier": "com.nandorojo.solito-tester"
-    },
-    "assetBundlePatterns": ["**/*"]
-  }
+export default {
+  name: 'solito-eas',
+  slug: 'solito-eas',
+  version: '1.0.0',
+  sdkVersion: '44.0.0',
+  scheme: 'solito-eas',
+  platforms: ['ios'],
+  runtimeVersion: '1',
+  updates: {
+    url: 'https://u.expo.dev/7e5b02b4-05a1-4c41-b65f-c9e09f685206',
+  },
+  ios: {
+    bundleIdentifier: 'com.nandorojo.solito-tester',
+  },
+  assetBundlePatterns: ['**/*'],
 }
